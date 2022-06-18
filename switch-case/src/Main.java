99ca146b38a4f4514c29964274c5096c6d6e46a8
Naman <mymail.naman@gmail.com>
2022-06-18 16:12:13 +0530

    Switch case

---
+++ switch-case/src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String day = "Friday";
+		
+		switch(day) {
+			case "Sunday" : System.out.println("It is "+day);
+			break;
+			case "Monday" : System.out.println("It is "+day);
+			break;
+			case "Tuesday" : System.out.println("It is "+day);
+			break;
+			case "Wednesday" : System.out.println("It is "+day);
+			break;
+			case "Thursday" : System.out.println("It is "+day);
+			break;
+			case "Friday" : System.out.println("It is "+day);
+			break;
+			case "Saturday" : System.out.println("It is "+day);
+			break;
+			default : System.out.println("It is not a day!");
+		
+		}
+
+	}
+
+}
